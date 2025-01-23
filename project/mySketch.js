f6a0d22089831701f15fdc86c968c6e6794ee7c0
Ms. Brooks <lbrooks@lwhs.org>
2025-01-23 23:21:25 +0000

    added a font

--- project/mySketch.js
+++ project/mySketch.js
@@ -1,8 +1,18 @@
+let f; 
+
+function preload(){
+  f = loadFont("ROMANTIC.TTF")
+}
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(200);
+  textFont(f)
+  textSize(22)
+  text("hello and welcome!", 100,100)
+
   
 }
