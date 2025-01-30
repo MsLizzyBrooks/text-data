843f197c011d354151d0ba6c8ca7fbd49719da00
Ms. Brooks <lbrooks@lwhs.org>
2025-01-30 22:06:40 +0000

    note

--- project/mySketch.js
+++ project/mySketch.js
@@ -6,13 +6,25 @@ function preload(){
 
 function setup() {
   createCanvas(400, 400);
-}
 
-function draw() {
   background(200);
   textFont(f)
   textSize(22)
-  text("hello and welcome!", 100,100)
+  text("do you like cabbage? left arrow = yes, right arrow = no", 100,100)
+}
+
+function draw() {
+ 
 
   
+}
+
+function keyPressed(){
+  if(keyCode === LEFT_ARROW){
+    fill("pink")
+  }  
+  if(keyCode === RIGHT_ARROW){
+    fill("green")
+  }  
+  ellipse(random(0,400),random(0,400),10,10)  
 }
